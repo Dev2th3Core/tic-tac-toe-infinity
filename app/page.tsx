fee02e4c63fa2386cc8333fb8fd605883c87739e
Rakshit4045 <rakshitshinde150@gmail.com>
2025-06-30 16:50:01 +0530

    Added footer with social links and copyright

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@
 // import TicTacToe from "../pages/components/tic-tac-toe";
 import Navbar from "@/app/components/Navbar";
 import TicTacToe from "./components/TicTacToe";
-import Link from "next/link";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div className="font-[family-name:var(--font-geist-sans)] pt-2 md:pt-4 flex flex-col justify-between h-screen">
       <Navbar />
       <TicTacToe />
-      <p className="mx-auto block mb-4">Built with 🤍 by <Link href="https://dev2th3core.site">Dev2th3Core</Link></p>
+      <Footer />
     </div>
   );
 } 
